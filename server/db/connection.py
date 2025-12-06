9e958be7a478870c8f7421ccc993736f10ec8597
Pavan Palve <pavan@Mac.lan>
2025-12-06 17:15:57 +0530

    chores: project setup complete

---
+++ server/db/connection.py
@@ -0,0 +1,4 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DBFILE = "expense_tracker_db.sqlite"
---
+++ server/models/sql_models.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, Integer, String, Numeric, Date, DateTime, ForeignKey, text
+from sqlalchemy.orm import relationship
+
+clas User(Bas)
